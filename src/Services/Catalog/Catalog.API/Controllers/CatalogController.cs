f8367edb1711c3e0f57a20a265a604f7097f26d8
Icaro Félix <ifeme08@gmail.com>
2023-12-18 01:16:33 -0300

    Feat: Catalog microservice already done.

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,123 @@
+﻿using Asp.Versioning;
+using Catalog.Application.Commands;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Specs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//using Common.Logging;
+//using Common.Logging.Correlation;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    public class CatalogController : ApiController
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<CatalogController> _logger;
+        //private readonly ICorrelationIdGenerator _correlationIdGenerator;
+
+        public CatalogController(
+            IMediator mediator, 
+            ILogger<CatalogController> logger
+            //ICorrelationIdGenerator correlationIdGenerator
+            )
+        {
+            _mediator = mediator;
+            _logger = logger;
+            //_correlationIdGenerator = correlationIdGenerator;
+            //_logger.LogInformation("CorrelationId {correlationId}:", _correlationIdGenerator.Get());
+        }
+
+        [HttpGet("[action]/{id}", Name = "GetProductById")]
+        [ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ProductResponse>> GetProductById(string id)
+        {
+            var query = new GetProductByIdQuery(id);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]/{productName}", Name = "GetProductByProductName")]
+        [ProducesResponseType(typeof(IList<ProductResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IList<ProductResponse>>> GetProductByProductName(string productName)
+        {
+            var query = new GetProductByNameQuery(productName);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAllProducts")]
+        [ProducesResponseType(typeof(IList<ProductResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IList<ProductResponse>>> GetAllProducts([FromQuery] CatalogSpecParams catalogSpecParams)
+        {
+            try
+            {
+                var query = new GetAllProductsQuery(catalogSpecParams);
+                var result = await _mediator.Send(query);
+                _logger.LogInformation("All products retrieved");
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An Exception has occured: {Exception}");
+                throw;
+            }
+        }
+
+        [HttpGet("GetAllBrands")]
+        [ProducesResponseType(typeof(IList<BrandResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IList<BrandResponse>>> GetAllBrands()
+        {
+            var query = new GetAllBrandsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAllTypes")]
+        [ProducesResponseType(typeof(IList<TypeResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IList<TypeResponse>>> GetAllTypes()
+        {
+            var query = new GetAllTypesQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]/{brand}", Name = "GetProductsByBrandName")]
+        [ProducesResponseType(typeof(IList<ProductResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IList<ProductResponse>>> GetProductsByBrandName(string brand)
+        {
+            var query = new GetProductsByBrandQuery(brand);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("CreateProduct")]
+        [ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ProductResponse>> CreateProduct([FromBody] CreateProductCommand productCommand)
+        {
+            var result = await _mediator.Send(productCommand);
+            return Ok(result);
+        }
+
+        [HttpPut("UpdateProduct")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] UpdateProductCommand productCommand)
+        {
+            var result = await _mediator.Send(productCommand);
+            return Ok(result);
+        }
+        [HttpDelete("{id}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            var query = new DeleteProductCommand(id);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+﻿using Catalog.Application;
+using Catalog.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
 
 namespace Catalog.API
 {
@@ -16,15 +21,27 @@ namespace Catalog.API
             services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(o => {
+                o.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "v1",
+                    Version = "v1",
+                });
+            });
+            services.AddInfrastructure();
+            services.AddApplication();
+            services.AddApiVersioning();
+            services.AddHealthChecks()
+                .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], "Catalog MongoDb HealthCheck", HealthStatus.Degraded);
         }
 
         public void Configure(WebApplication app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
             }
 
             app.UseHttpsRedirection();
@@ -32,6 +49,11 @@ namespace Catalog.API
             app.UseAuthorization();
 
             app.MapControllers();
+            app.MapHealthChecks("/heath", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
             
         }
     }
